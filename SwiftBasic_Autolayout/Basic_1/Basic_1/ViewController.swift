27dd14d3b46b9f6356b1fb1133e1f5385e835c1b
System Administrator <root@gimjunseog-ui-MacBookPro.local>
2020-03-03 00:06:53 +0900

    SwiftBasic_Autolaout 20200302

---
+++ SwiftBasic_Autolayout/Basic_1/Basic_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Basic_1
+//
+//  Created by 김준석 on 2020/03/02.
+//  Copyright © 2020 Kim JunSeok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
