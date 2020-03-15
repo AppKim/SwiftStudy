e6d0da9041d2220961f4f9101edd45561a92f961
System Administrator <root@gimjunseog-ui-MacBookPro.local>
2020-03-15 23:40:12 +0900

    SwiftBasic_Autolayout 20200315

---
+++ SwiftBasic_Autolayout/Compression_resistance/Compression_resistance/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Compression_resistance
+//
+//  Created by 김준석 on 2020/03/15.
+//  Copyright © 2020 Kim JunSeok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
