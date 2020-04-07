fb947e72d9c6c14d7a40bedfc22a6dcef9e8428f
System Administrator <root@gimjunseog-ui-MacBookPro.local>
2020-04-07 23:01:11 +0900

    SwiftBasic_Autolayout 20200407

---
+++ SwiftBasic_Autolayout/Floating_Button/Floating_Button/FloatingButtonListViewController.swift
@@ -0,0 +1,31 @@
+//
+//  FloationgButtonListViewController.swift
+//  Floating_Button
+//
+//  Created by 김준석 on 2020/04/07.
+//  Copyright © 2020 Kim JunSeok. All rights reserved.
+//
+
+import UIKit
+
+class FloatingButtonListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
--- SwiftBasic_Autolayout/Floating_Button/Floating_Button/ViewController.swift
+++ SwiftBasic_Autolayout/Floating_Button/Floating_Button/ViewController.swift
@@ -14,6 +14,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "shopPopup" {
+            
+            // FloatingButtonListViewControllerを取得
+            // 쇼로연결했을때 세그웨이옵션에서 에니메이션 제거해줘야지 위에서올라오는 애니메이션사라짐
+            let floatingVC = segue.destination as! FloatingButtonListViewController
+            // FloatingButtonListViewControllerのモーダルを透明にする
+            floatingVC.modalPresentationStyle = .overCurrentContext
+            
+            
+        }
+        
+    }
 
 
 }
