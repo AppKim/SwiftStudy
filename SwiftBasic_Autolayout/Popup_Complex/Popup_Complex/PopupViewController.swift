857e71b52f486ad9ce468b38d8c91e3313348598
System Administrator <root@gimjunseog-ui-MacBookPro.local>
2020-04-14 23:23:20 +0900

    SwiftBasic_Autolayout 20200414

---
+++ SwiftBasic_Autolayout/Popup_Complex/Popup_Complex/PopupViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PopupViewController.swift
+//  Popup_Complex
+//
+//  Created by 김준석 on 2020/04/14.
+//  Copyright © 2020 Kim JunSeok. All rights reserved.
+//
+
+import UIKit
+
+class PopupViewController: UIViewController {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ SwiftBasic_Autolayout/Popup_Complex/Popup_Complex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Popup_Complex
+//
+//  Created by 김준석 on 2020/04/14.
+//  Copyright © 2020 Kim JunSeok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
