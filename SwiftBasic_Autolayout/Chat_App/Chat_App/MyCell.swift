f40e2107c84ea84478a6c9f702e83148353fad10
System Administrator <root@gimjunseog-ui-MacBookPro.local>
2020-04-24 22:46:46 +0900

    SwiftBasic_Autolayout 20200424

---
+++ SwiftBasic_Autolayout/Chat_App/Chat_App/MyCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyCell.swift
+//  Chat_App
+//
+//  Created by 김준석 on 2020/04/24.
+//  Copyright © 2020 Kim JunSeok. All rights reserved.
+//
+
+import UIKit
+
+class MyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
