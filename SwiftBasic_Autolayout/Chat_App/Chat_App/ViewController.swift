80086a9300e9e019bdc5f34ade335d4ab678f9fb
System Administrator <root@gimjunseog-ui-MacBookPro.local>
2020-04-25 23:32:21 +0900

    SwiftBasic_Autolayout 20200425

--- SwiftBasic_Autolayout/Chat_App/Chat_App/ViewController.swift
+++ SwiftBasic_Autolayout/Chat_App/Chat_App/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var chatTableView: UITableView!
+    @IBOutlet weak var inputTextView: UITextView!
+    @IBOutlet weak var inputViewBottomMargin: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // 使用するセルを登録しなければならないため
+        chatTableView.register(UINib(nibName: "MyCell", bundle: nil), forCellReuseIdentifier: "myCell")
+        chatTableView.register(UINib(nibName: "YourCell", bundle: nil), forCellReuseIdentifier: "yourCell")
+        
+        // キーボードの状態を知らせてくれる
+        // 키보드 관련 옵저버 설정을 해야 함
+        // 키보드가 올라올때
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        // 키보드가 내려올때
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
     }
-
+    
+    @objc func keyboardWillShow(noti: Notification) {
+        
+    }
+    
+    @objc func keyboardWillHide(noti: Notification) {
+        
+    }
+    
+    
+    @IBAction func sendString(_ sender: Any) {
+        
+    }
+    
 
 }
 
